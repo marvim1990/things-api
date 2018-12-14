fda403a8067d59d9cc15f45b99142e61ac74de86
murilosouza <murilobarbozasouza@gmail.com>
2018-12-14 15:33:31 -0200

    database adicionado

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ global.Uni_KEY = 'bed90f62-2a88-1023-379b-23f001d7d852';
 global.EMAIL_TMPL = 'Olá <strong>{0}</strong>, seja bem vindo à Comunidade Things';
 
 module.exports = {
-    connectionString : 'mongodb://mirtilio:2u57vcd3@ds117834.mlab.com:17834/things_pro',
+    connectionString : 'mongodb://murilobs:yt790ch22gu@kamino.mongodb.umbler.com:36004/things_db',
     sendgridkey: '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',
     connectionAzure : 'DefaultEndpointsProtocol=https;AccountName=thingstorage;AccountKey=9jDiI1+bNc31ihNhLv9URhd//Cki/i6oaur/rBAfNwv1Z2fpKuUfi2NrXRkAMmHWSuOH8m+eiPNT8aVzkDbk3w==;EndpointSuffix=core.windows.net'
 }
