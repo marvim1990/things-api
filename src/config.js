795f4c7d837d5edb7bf152293d5e7ab214affead
murilosouza <murilobarbozasouza@gmail.com>
2019-01-09 14:12:42 -0200

    mudanca na chave de acesso

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 global.Uni_KEY = 'CHAVE AQUI';
-global.EMAIL_TMPL = 'Olá <strong>{0}</strong>, seja bem vindo à Comunidade Things';
+global.EMAIL_TMPL = 'Olá <strong>{0}</strong>, seja bem vindo';
 
 module.exports = {
     connectionString : 'String de conexão aqui',
     sendgridkey: 'chave sendgrid',
-    connectionAzure : 'conexão com Azure Storage'
+    connectionAzure : 'conexão com Azure'
 }
