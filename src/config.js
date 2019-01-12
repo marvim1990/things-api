9e0db17d2198f25e61015c71af7ec5a7ba4e799c
murilosouza <murilobarbozasouza@gmail.com>
2019-01-12 15:25:06 -0200

    mudanca auth 2

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
-global.Uni_KEY = 'bed90f62-2a88-1023-379b-23f001d7d852';
+global.Uni_KEY = '** CHAVE AQUI **';
 global.EMAIL_TMPL = 'Olá <strong>{0}</strong>, seja bem vindo à Comunidade Things';
 
 module.exports = {
-    connectionString : 'mongodb://murilobs:yt790ch22gu@kamino.mongodb.umbler.com:36004/things_db',
-    sendgridkey: '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',
-    connectionAzure : 'DefaultEndpointsProtocol=https;AccountName=thingstorage;AccountKey=9jDiI1+bNc31ihNhLv9URhd//Cki/i6oaur/rBAfNwv1Z2fpKuUfi2NrXRkAMmHWSuOH8m+eiPNT8aVzkDbk3w==;EndpointSuffix=core.windows.net'
+    connectionString : '** CHAVE AQUI **',
+    sendgridkey: '** CHAVE AQUI **',
+    connectionAzure : '** CHAVE AQUI **'
 }
